c586e4297f961c30af927190b3247ece1a97efde
LAHAXE Arnaud <arnaud.lahaxe@boursorama.fr>
2016-10-07 10:25:17 +0200

    remove views and assets management

--- api/responses/badRequest.js
+++ api/responses/badRequest.js
@@ -37,28 +37,7 @@ module.exports = function badRequest(data, options) {
   if (sails.config.environment === 'production') {
     data = undefined;
   }
-
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  else return res.guessView({ data: data }, function couldNotGuessView () {
-    return res.jsonx(data);
-  });
-
+  
+  return res.jsonx(data);
 };
 
